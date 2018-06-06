13a3a0dd43420cf1083e96d92d9e8738ed3c2c6d
Jez Templeton <j.templeton@bom.gov.au>
2018-06-06 21:38:25 +1000

    Updated mock UI to more reflect a realistic interface.

--- src/components/UITest/UITest.tsx
+++ src/components/UITest/UITest.tsx
@@ -3,14 +3,33 @@ import styled from 'theme'
 
 import { IProps, IDefaultProps, IState } from './UITest.d'
 
-const Content = styled.div`
+const UIContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  height: 100vh;
+`
+const RoomDescription = styled.div`
   margin: 2rem;
 `
 
-const Quote = styled.blockquote`
-  font-size: 2rem;
+const ActionPanel = styled.div`
+  margin: 2rem;
+`
+
+const StatusBar = styled.div`
+  margin: auto 2rem 2rem 2rem;
+`
+
+const AreaName = styled.blockquote`
+  font-size: 1.25rem;
   margin: 1rem 0;
 `
+
+const RoomName = styled.blockquote`
+  font-size: 1.5rem;
+  margin: 1rem 0;
+`
+
 
 class UITest extends React.PureComponent<IProps, IState> {
   static defaultProps: Partial<IDefaultProps> = {}
@@ -18,14 +37,21 @@ class UITest extends React.PureComponent<IProps, IState> {
   render() {
     // const {} = this.props as PropsWithDefaults
     return (
-      <Content>
-        <p>Alone, you wait, ready to cross.</p>
-        <p>On a nearby corner a violin sings; the cadence of its bow dancing in time with the rhythm of your thoughts.</p>
-        <p>A <strong>moment of inchoate longing</strong> builds inside you, wishing to step into that oncoming rush and </p>
-        <Quote>allowing it to carry you away to a quieter place,</Quote>
-        <p>to that inevitable finality where all music stops (<em>where breath ceases</em>)</p>
-        <p>the place where I wait for you.</p>
-      </Content>
+      <UIContainer>
+        <RoomDescription>
+          <AreaName>Villespont, <em>the City of Bridges</em></AreaName>
+          <RoomName><strong>Lower Arch Station</strong>, the demilitarised zone</RoomName>
+          <p>On a nearby corner a violin sings; the cadence of its bow dancing in time with the rhythm of your thoughts.</p>
+          <p>You feel the weight of the bridges above you.</p>
+        </RoomDescription>
+        <ActionPanel>
+          A young lady is here. She seems bored.
+        </ActionPanel>
+        <StatusBar>
+          <p>It is late in the day.</p>
+          <p>You feel oppressed, and hungry.</p>
+        </StatusBar>
+      </UIContainer>
     )
   }
 }
